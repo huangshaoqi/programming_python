929db4abd33421140ce39c6ffd2a678eedf8d831
huangshaoqi <290678391@qq.com>
2018-08-28 00:09:49 +0800

    表示记录1
    Signed-off-by: huangshaoqi <290678391@qq.com>

--- Part_1/py1.py
+++ Part_1/py1.py
@@ -1,4 +1,32 @@
+"""
+表示记录
+"""
+# 使用List
 bob = ['Bob Smith', 42, 30000, 'software']
 sue = ['Sue Jones', 45, 40000, 'hardware']
 
 print(bob[0], sue[2])
+print(bob[0].split()[-1])
+sue[2] *= 1.25
+print(sue)
+
+# 数据库列表
+people = [bob, sue]
+for person in people:
+    print(person)
+
+print(people[1][0])
+for person in people:
+    print(person[0].split()[-1])
+    person[2] *= 1.20
+
+for person in people:
+    print(person[2])
+
+pays = [person[2] for person in people]
+print(pays)
+
+pays = map((lambda x: x[2]), people)
+print(list(pays))
+
+print(sum(person[2] for person in people))
